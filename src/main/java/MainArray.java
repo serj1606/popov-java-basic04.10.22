ef56e896334f8d4b8c85f71a1085d1bdd3dc4184
Sergey Popov <popow_sv@ukr.net>
2022-12-12 17:26:48 +0200

    My homework Unit Testing third attempt

--- src/main/java/MainArray.java
+++ src/main/java/MainArray.java
@@ -1,28 +1,49 @@
-import java.util.Arrays;
-
 public class MainArray {
     public static void main(String[] args) {
 
     }
 
-    public static int countArray(int[][] arrayOne1) {
-        int sum = 0;
-        int totalSum = 0;
-        for (int i = 0; i < arrayOne1.length; i++) {
-            for (int j = 0; j < arrayOne1.length; j++) {
-                sum += arrayOne1[i][j];
-
+    public static double averageArray(int[][] array) {
+        double sum = 0.0;
+        double average = 0.0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                sum += array[i][j];
             }
-            totalSum = sum / (arrayOne1.length * arrayOne1.length);
+            average = sum / (array.length * array.length);
         }
-
-        return totalSum;
+        return average;
     }
 
-    public static int arraySquare(int[][] arrayOne1) {
-        for (int i = 0; i < arrayOne1.length; i++) {
-            for (int j = 0; j < arrayOne1[i].length - 1; ) {
-                if (arrayOne1.length == arrayOne1[j].length) {
+    public static int arraySquare(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length - 1;j++) {   // не могу перебрать столбик
+                if (array.length == array[j].length) { //не хочет работать со столбиком, работает только с первой строчкой массива
+                    System.out.println("Матриця квадратна");
+                    return 1;
+                } else {
+                    System.out.println("Матриця не квадратна");
+                    return 2;
+                }
+            }
+        }return 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+    public static int arraySquare(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length - 1; ) {
+                if (array.length == array[j].length) {
                     System.out.println("Матрица квадратная");
                     return 1;
                 } else {
@@ -32,5 +53,4 @@ public class MainArray {
             }
         }
         return 0;
-    }
-}
+    }*/
--- src/test/java/MainArrayTest.java
+++ src/test/java/MainArrayTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 
 public class MainArrayTest {
-    int[][] arrayOne1 = new int[][]{
+    int[][] array = new int[][]{
             {11, 2, 4},
             {6, 3, 2},
             {2, 1, 5}
@@ -11,12 +11,12 @@ public class MainArrayTest {
 
     @Test
     public void test1() {
-        Assertions.assertEquals(4, MainArray.countArray(arrayOne1));
+        Assertions.assertEquals(4, MainArray.averageArray(array));
     }
 
     @Test
     public void test2() {
-        Assertions.assertEquals(1, MainArray.arraySquare(arrayOne1));
+        Assertions.assertEquals(1, MainArray.arraySquare(array));
     }
 }
 
